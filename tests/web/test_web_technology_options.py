2130d2e95d06c696333a858c910123e89dee2244
Christopher Fregly <31714410+freglyc@users.noreply.github.com>
2018-06-19 16:12:46 -0500

    Fixed import and spacing issues

--- tests/web/test_web_technology_options.py
+++ tests/web/test_web_technology_options.py
@@ -1,13 +1,13 @@
+from test.markers import web, test_case
 
 @test_case('')
 @web
 def test_view_technology_options(web_base_url, selenium):
     """Tests ability to view to technology options page."""
-    
     # GIVEN: On the OpenStax homepage
 
     # WHEN: Click "Technology"
-
     # AND: Click "Technology Options"
 
     # THEN: User can view the technology options page
+    
