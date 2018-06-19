8ac66e9a6fb00ac93a60f808dd0dac9d9859112f
Christopher Fregly <31714410+freglyc@users.noreply.github.com>
2018-06-19 16:07:46 -0500

    Fixed import and spacing issues

--- tests/web/test_web_about_us.py
+++ tests/web/test_web_about_us.py
@@ -1,9 +1,9 @@
+from test.markers import web, test_case
 
 @test_case('')
 @web
 def test_view_about_us(web_base_url, selenium):
     """Tests ability go to about us page."""
-    
     # GIVEN: On the OpenStax homepage
 
     # WHEN: Click "About Us"
@@ -15,7 +15,6 @@ def test_view_about_us(web_base_url, selenium):
 @web
 def test_view_about_us_hyperlinks(web_base_url, selenium):
     """Tests hyperlinks are present in about us page."""
-    
     # GIVEN: On the OpenStax homepage
 
     # WHEN: Click "About Us"
@@ -27,11 +26,9 @@ def test_view_about_us_hyperlinks(web_base_url, selenium):
 @web
 def test_view_team_members(web_base_url, selenium):
     """Tests ability to view team members."""
-    
     # GIVEN: On the OpenStax homepage
 
     # WHEN: Click "About Us"
 
     # THEN: Click a team member photo, a window will appear on top with the introduction
-
     # AND: Click the x and the window introduction will close
