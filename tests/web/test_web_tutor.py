abd937c6acdeae15b11cb571b9861e6ea0c94cc5
Christopher Fregly <31714410+freglyc@users.noreply.github.com>
2018-06-19 16:13:09 -0500

    Fixed import and spacing issues

--- tests/web/test_web_tutor.py
+++ tests/web/test_web_tutor.py
@@ -1,15 +1,13 @@
+from test.markers import web, test_case
 
 @test_case('')
 @web
 def test_view_tutor(web_base_url, selenium):
     """Tests ability to view tutor."""
-    
     # GIVEN: On the OpenStax homepage
 
     # WHEN: Click "Technology"
-
     # AND: Click "About OpenStax Tutor"
-
     # AND: Click "Get Started"
 
     # THEN: User is on accounts if not logged in, tutor if logged in
