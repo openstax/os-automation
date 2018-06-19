deeabd4cc60e74946c0160db3124162881e6de3a
Christopher Fregly <31714410+freglyc@users.noreply.github.com>
2018-06-19 16:09:28 -0500

    Fixed spacing and import issues

--- tests/web/test_web_home.py
+++ tests/web/test_web_home.py
@@ -1,13 +1,12 @@
+from test.markers import web, test_case
 
 @test_case('')
 @web
 def test_view_home(web_base_url, selenium):
     """Tests ability view homepage from other pages."""
-    
     # GIVEN: On the OpenStax homepage
 
     # WHEN: Click "About Us"
-
     # AND: Click the OpenStax logo on the upper left corner
 
     # THEN: On homepage
