2175d789fd723fb9a25286d723645762168ee9a0
Christopher Fregly <31714410+freglyc@users.noreply.github.com>
2018-06-19 16:09:49 -0500

    Fixed spacing and import issues

--- tests/web/test_web_openstax_partners.py
+++ tests/web/test_web_openstax_partners.py
@@ -1,13 +1,12 @@
+from test.markers import web, test_case
 
 @test_case('')
 @web
 def test_view_openstax_partners(web_base_url, selenium):
     """Tests ability to view openstax partners page."""
-    
     # GIVEN: On the OpenStax homepage
 
     # WHEN: Click "Technology"
-
     # AND: Click "OpenStax Partners"
 
     # THEN: User is navigated to the OpenStax partners page
