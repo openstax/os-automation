20f9ce8ef30a408ab7b24bdc11a22b45c7968f01
TheraFu <therafuminqi@gmail.com>
2018-06-26 16:48:01 -0500

    update test descriptions at the tests original places

--- tests/tutor/test_tutor_course_roster.py
+++ tests/tutor/test_tutor_course_roster.py
@@ -8,12 +8,10 @@ from tests.markers import expected_failure, nondestructive, test_case, tutor
 @tutor
 def test_add_a_new_section(tutor_base_url, selenium, teacher):
     """Add a new section in course roster."""
-    # GIVEN: Logged into tutor as a teacher
-    # AND: has a existing course
+    # GIVEN: Logged into Tutor as a teacher
+    # AND: User is at the "Course Roster" page
 
-    # WHEN: Select a course
-    # AND: click "course roster"
-    # AND: Click "add a section"
+    # WHEN: Click "add a section"
     # AND: Enter a section name, click enter
 
     # THEN: A new section should appear
@@ -24,12 +22,10 @@ def test_add_a_new_section(tutor_base_url, selenium, teacher):
 @tutor
 def test_add_a_new_instructor(tutor_base_url, selenium, teacher):
     """Add a new instruction in course roster."""
-    # GIVEN: Logged into tutor as a teacher
-    # AND: has a existing course
+    # GIVEN: Logged into Tutor as a teacher
+    # AND: User is at the "Course Roster" page
 
-    # WHEN: Select a Tutor course
-    # AND: Click "course roster"
-    # AND: Click "add a new instructor"
+    # WHEN: Click "add a new instructor"
     # AND: Add instructor name, click enter
 
     # THEN: A url link that instructor could use to join this course is shown
@@ -41,12 +37,10 @@ def test_add_a_new_instructor(tutor_base_url, selenium, teacher):
 @tutor
 def test_roster_switch_between_sections(tutor_base_url, selenium, teacher):
     """Switch view between sections in course roster."""
-    # GIVEN: Logged into tutor as a teacher
-    # AND: has a existing course
+    # GIVEN: Logged into Tutor as a teacher
+    # AND: User is at the "Course Roster" page
 
-    # WHEN: Select a Tutor course
-    # AND: Click "course roster"
-    # AND: Click on another section
+    # WHEN: Click on another section
 
     # THEN: user is switched to the other section
 
@@ -56,12 +50,10 @@ def test_roster_switch_between_sections(tutor_base_url, selenium, teacher):
 @tutor
 def test_rename_a_section(tutor_base_url, selenium, teacher):
     """Rename a section in course roster."""
-    # GIVEN: Logged into tutor as a teacher
-    # AND: has a existing course
+    # GIVEN: Logged into Tutor as a teacher
+    # AND: User is at the "Course Roster" page
 
-    # WHEN: Select a Tutor course
-    # AND: Click "course roster"
-    # AND: Click "rename"
+    # WHEN: Click "rename"
     # AND: Type in new name, click enter
 
     # THEN: Section is renamed
@@ -72,12 +64,10 @@ def test_rename_a_section(tutor_base_url, selenium, teacher):
 @tutor
 def test_delete_a_section(tutor_base_url, selenium, teacher):
     """Delete a section in course roster."""
-    # GIVEN: Logged into tutor as a teacher
-    # AND: has a existing course with two sections
+    # GIVEN: Logged into Tutor as a teacher
+    # AND: User is at the ""Course Roster"" page
 
-    # WHEN: Select a Tutor course
-    # AND: Click "course roster"
-    # AND: Click "delete section"
+    # WHEN: Click "delete section"
     # AND: Click "yes"
 
     # THEN: the section is deleted
@@ -89,12 +79,35 @@ def test_delete_a_section(tutor_base_url, selenium, teacher):
 @tutor
 def test_cancel_delete_a_section(tutor_base_url, selenium, teacher):
     """Cancel deleting a section in course roster."""
-    # GIVEN: Logged into tutor as a teacher
-    # AND: has a existing course with two sections
+    # GIVEN: Logged into Tutor as a teacher
+    # AND: User is at the "Course Roster" page
 
-    # WHEN:  Select a Tutor course
-    # AND: Click "course roster"
-    # AND: Click "delete a section"
+    # WHEN: Click "delete a section"
     # AND: Click "cancel"
 
     # THEN: The section should not be deleted
+
+
+@test_case('')
+@tutor
+def test_cancel_renaming_a_section(tutor_base_url, selenium, teacher):
+    """Cancel renaming a section in course roster."""
+    # GIVEN: Logged into Tutor as a teacher
+    # AND: User is at the "Course Roster" page
+
+    # WHEN: Click "rename a section"
+    # AND: Click "cancel"
+
+    # THEN: The section name should not change
+
+
+@test_case('')
+@tutor
+def test_edit_student_id(tutor_base_url, selenium, teacher):
+    """Edit student id in course roster."""
+    # GIVEN: Logged into Tutor as a teacher
+    # AND: User is at the "Course Roster" page
+
+    # WHEN: Click on the little pencil sign near student id
+
+    # THEN: the student's id is edited
