3b0611341be2048c82bedf7552523b40ff5c41ef
TheraFu <therafuminqi@gmail.com>
2018-07-05 14:01:46 -0500

    Flake changes

--- pages/tutor/home.py
+++ pages/tutor/home.py
@@ -1,21 +1,19 @@
 """Home page objects."""
 
-from pypom import Region
 from selenium.webdriver.common.by import By
 
-from pages.tutor.base import TutorBase
 from pages.accounts.home import AccountsHome
+from pages.tutor.base import TutorBase
 from pages.tutor.dashboard import TutorDashboard
 
 
 class TutorHome(TutorBase):
-    """Tutor home page for users that are not logged in"""
+    """Tutor home page for users that are not logged in."""
 
     _login_btn_locator = (By.CSS_SELECTOR, '.login')
 
     def log_in(self, user, password):
         """Log into the site with a specific user."""
-
         self.find_element(*self._login_btn_locator).click()
         AccountsHome(self.driver).service_log_in(user, password)
         self.wait.until(lambda _: self.logged_in)
