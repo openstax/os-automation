ffadf6f77cba51af6f2e3a5668d95f4ff67f71b8
Greg Fitch <greg.fitch@rice.edu>
2018-07-13 11:27:05 -0500

    rework About Us page object for access and style reqs

--- pages/web/about_us.py
+++ pages/web/about_us.py
@@ -1,22 +1,25 @@
 """OpenStax About Us Page."""
 
+from time import sleep
+
 from pypom import Region
 from selenium.webdriver.common.by import By
+
 from pages.web.base import WebBase
 
 
 class AboutUs(WebBase):
-    """About us page."""
+    """About Us page."""
 
     _root_locator = (By.ID, 'main')
-    _splash_locator = (By.CLASS_NAME, 'w-cards')
+    _splash_locator = (By.CSS_SELECTOR, '.w-cards h1')
     _about_locator = (By.CLASS_NAME, 'meta')
-    _people_locator = (By.XPATH, "//*[@id='main']/div/div[3]")
+    _people_locator = (By.CSS_SELECTOR, "#main .boxed")
 
     @property
     def splash(self):
         """Splash banner region."""
-        return self.find_element(*self._splash_locator)
+        return self.find_element(*self._splash_locator).text
 
     @property
     def about(self):
@@ -33,11 +36,10 @@ class AboutUs(WebBase):
     class About(Region):
         """About region."""
 
-        _about_links_locator = (By.CSS_SELECTOR, 'a')
-
-        def get_links(self):
-            """Gets all the links in this section."""
-            return self.find_elements(*self._about_links_locator)
+        _mission_statement_locator = (By.CSS_SELECTOR, 'p:first-child')
+        _promote_oer_locator = (By.CSS_SELECTOR, '[href*=".pdf"]')
+        _support_link_locator = (By.PARTIAL_LINK_TEXT, 'Support')
+        _faq_link_locator = (By.CSS_SELECTOR, '[href="/faq"]')
 
     class People(Region):
         """People region."""
@@ -59,42 +61,45 @@ class AboutUs(WebBase):
             items = self.find_elements(*self._advisors_headshots_locator)
             return [self.Headshot(self, el) for el in items]
 
-    class Headshot(Region):
-        """Headshot region."""
+        class Headshot(Region):
+            """Headshot region."""
 
-        _name_locator = (By.CSS_SELECTOR, '.picture-area .name')
-        _description_locator = (By.CLASS_NAME, 'description')
-        _photo_locator = (By.CSS_SELECTOR, 'img')
-        _click_headshot_locator = (By.CLASS_NAME, 'picture-area')
-        _exit_locator = (By.CSS_SELECTOR, '.btn.dismiss')
+            _name_locator = (By.CSS_SELECTOR, '.picture-area .name')
+            _description_locator = (By.CLASS_NAME, 'description')
+            _photo_container_locator = (By.CLASS_NAME, 'picture-area')
+            _photo_locator = (By.CSS_SELECTOR, 'img')
+            _exit_locator = (By.CSS_SELECTOR, '.btn.dismiss')
 
-        @property
-        def name(self):
-            """Name of the person."""
-            return self.find_element(*self._name_locator).text
+            @property
+            def name(self):
+                """Return the team member's name."""
+                return self.find_element(*self._name_locator).text
 
-        @property
-        def description(self):
-            """Description of the person."""
-            return self.find_element(*self._description_locator).text
+            @property
+            def description(self):
+                """Return the team member's biography."""
+                return self.find_element(*self._description_locator).text
 
-        @property
-        def photo_present(self):
-            """Returns true/false depening on if photo is present or not."""
-            if self.find_element(*self._photo_locator):
-                return True
-            else:
-                return False
+            @property
+            def photo_present(self):
+                """Return true if a photo is present."""
+                el = self.find_element(*self._photo_container_locator)
+                return 'has-picture' in el.get_attribute('class')
 
-        @property
-        def get_photo(self):
-            """Photo of the person if present."""
-            return self.find_element(*self._photo_locator)
+            @property
+            def get_photo(self):
+                """Return the team member's photo source URL."""
+                el = self.find_element(*self._photo_locator)
+                return el.get_attribute('src')
 
-        def click_headshot(self):
-            """Clicks the headshot to view description."""
-            self.find_element(*self._click_headshot_locator).click()
+            def open_description(self):
+                """Open the description box."""
+                self.find_element(*self._photo_container_locator).click()
+                sleep(0.25)
+                return self
 
-        def close_description(self):
-            """Closes the description box."""
-            self.find_element(*self._exit_locator).click()
+            def close_description(self):
+                """Close the description box."""
+                self.find_element(*self._exit_locator).click()
+                sleep(0.25)
+                return self
