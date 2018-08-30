2da52c2521e1bda34312a73a48dfc59376080516
Greg Fitch <greg.fitch@rice.edu>
2018-08-30 16:53:26 -0500

    Web regions, base and new WebHome (#112)
    * rewrite the shared footer
    
    * flipped the osnav and webnav
    
    * updated the long nav
    
    * tack on a region for stickies - just on home for now but it shows up on other web pages
    
    * and toss in the base and home models

--- regions/web/footer.py
+++ regions/web/footer.py
@@ -1,4 +1,4 @@
-"""Web's shared footer region."""
+"""OpenStax Web's shared footer region."""
 
 from time import sleep
 
@@ -9,125 +9,131 @@ from selenium.webdriver.common.by import By
 class Footer(Region):
     """OpenStax Web footer region."""
 
-    _root_locator = (By.TAG_NAME, 'footer')
+    _root_locator = (By.ID, 'footer')
+    _box_locator = (By.CLASS_NAME, 'hero-quote')
+    _directory_locator = (By.CSS_SELECTOR, '[role=contentinfo]')
+    _social_locator = (By.CLASS_NAME, 'social')
 
     @property
-    def is_footer_displayed(self):
+    def is_displayed(self):
         """Return true if the footer is currently displayed."""
         return self.is_displayed
 
     @property
-    def statements(self):
-        """Return Statement region."""
-        return self.Statements(self)
+    def box(self):
+        """Access the box region."""
+        region_root = self.find_element(*self._box_locator)
+        return self.Box(self, region_root)
 
     @property
-    def content_links(self):
-        """Return ContentLinks region."""
-        return self.ContentLinks(self)
+    def directory(self):
+        """Access the directory."""
+        region_root = self.find_element(*self._directory_locator)
+        return self.Directory(self, region_root)
 
     @property
-    def social_links(self):
-        """Return SocialLinks region."""
-        return self.SocialLinks(self)
+    def social(self):
+        """Access the social services."""
+        region_root = self.find_element(*self._social_locator)
+        return self.SocialLinks(self, region_root)
 
-    class Statements(Region):
-        """Statements region."""
+    class Box(Region):
+        """The footer's statement box."""
 
-        _hero_quote_locator = (By.CLASS_NAME, 'hero-quote')
-        _nonprofit_locator = (By.CSS_SELECTOR, "[role*='contentinfo'] p")
-        _copyright_locator = (By.CSS_SELECTOR, "[role*='contentinfo'] p")
-        _trademark_locator = (By.CSS_SELECTOR, 'ap-html')
-
-        def hero_statement(self):
+        @property
+        def statement(self):
             """Return the hero banner statement."""
-            return self.find_element(*self._hero_quote_locator).text
+            return self.root.text
 
-        def nonprofit_statement(self):
-            """Return the nonprofit statement."""
-            return self.find_elements(*self._nonprofit_locator)[0].text
+    class Directory(Region):
+        """The site map directory links."""
 
-        def copyright_statement(self):
-            """Return the copyright statement."""
-            return self.find_elements(*self._copyright_locator)[1].text
+        _license_locator = (By.CSS_SELECTOR, '[href$=license]')
+        _terms_of_use_locator = (By.CSS_SELECTOR, '[href$=tos]')
+        _privacy_policy_locator = (By.CSS_SELECTOR, '[href$=privacy-policy]')
+        _accessibility_statement_locator = (By.CSS_SELECTOR,
+                                            '[href*=accessibility]')
+        _open_source_locator = (By.CSS_SELECTOR, '[href*=github]')
+        _contact_us_locator = (By.CSS_SELECTOR, '[href$=contact]')
+        _press_locator = (By.CSS_SELECTOR, '[href*=press]')
+        _newsletter_locator = (By.LINK_TEXT, 'Newsletter')
+        _nonprofit_statement_locator = (By.CSS_SELECTOR, 'p:first-child')
+        _copyright_statement_locator = (By.CSS_SELECTOR, 'p:nth-child(2)')
+        _ap_statement_locator = (By.TAG_NAME, 'ap-html')
 
-        def trademark_statement(self):
-            """Return the trademark statement."""
-            return self.find_element(*self._trademark_locator).text
-
-    class ContentLinks(Region):
-        """Content Links region."""
-
-        _license_locator = (By.CSS_SELECTOR, "a[href*='license']")
-        _terms_of_service_locator = (By.CSS_SELECTOR, "a[href*='tos']")
-        _privacy_policy_locator = (
-            By.CSS_SELECTOR, "a[href*='privacy-policy']")
-        _accessibility_statement_locator = (
-            By.CSS_SELECTOR, "a[href*='accessibility-statement']")
-        _open_source_locator = (
-            By.CSS_SELECTOR, "a[href*='https://github.com/openstax']")
-        _contact_us_locator = (By.CSS_SELECTOR, "a[href*='contact']")
-        _press_locator = (By.CSS_SELECTOR, "a[href*='press']")
-        _newsletter_locator = (By.PARTIAL_LINK_TEXT, "Newsletter")
-
-        def go_to_license(self):
+        def view_licensing(self):
             """Go to the website license page."""
             self.find_element(*self._license_locator).click()
-            sleep(1)
+            sleep(1.0)
             from pages.web.license import License
             return License(self.driver)
 
-        def go_to_tos(self):
-            """View the terms of service."""
-            self.find_element(*self._terms_of_service_locator).click()
-            sleep(1)
+        def view_the_terms_of_use(self):
+            """View the terms of use."""
+            self.find_element(*self._terms_of_use_locator).click()
+            sleep(1.0)
             from pages.web.terms import Terms
             return Terms(self.driver)
 
-        def go_to_privacy_policy(self):
+        def view_the_privacy_policy(self):
             """View the privacy policy."""
             self.find_element(*self._privacy_policy_locator).click()
-            sleep(1)
+            sleep(1.0)
             from pages.web.privacy import PrivacyPolicy
             return PrivacyPolicy(self.driver)
 
-        def go_to_accessibility_statement(self):
+        def view_the_accessibility_statement(self):
             """View the accessibility statement."""
             self.find_element(*self._accessibility_statement_locator).click()
-            sleep(1)
+            sleep(1.0)
             from pages.web.accessibility import Accessibility
             return Accessibility(self.driver)
 
-        def go_to_open_source(self):
+        def view_the_code(self):
             """Open GitHub and view the OpenStax repositories."""
             self.find_element(*self._opens_source_locator).click()
-            sleep(1)
+            sleep(1.0)
             from pages.github.home import GitHub
             return GitHub(self.driver)
 
-        def go_to_contact_us(self):
+        def go_to_the_contact_form(self):
             """Go to the contact form."""
             self.find_element(*self._contact_us_locator).click()
-            sleep(1)
+            sleep(1.0)
             from pages.web.contact import Contact
             return Contact(self.driver)
 
-        def go_to_press(self):
+        def view_press_releases(self):
             """View the press page."""
             self.find_element(*self._press_locator).click()
-            sleep(1)
+            sleep(1.0)
             from pages.web.press import Press
             return Press(self.driver)
 
-        def go_to_newsletter(self):
+        def go_to_the_newsletter_signup_form(self):
             """Go to the newsletter signup."""
             self.find_element(*self._newsletter_locator).click()
-            sleep(1)
+            sleep(1.0)
             from pages.web.newsletter import NewsletterSignup
             return NewsletterSignup(self.driver)
 
-    class SocialLinks(Region):
-        """Social Links region."""
+        @property
+        def organization(self):
+            """Return the OpenStax tax status statement."""
+            return self.find_element(*self._nonprofit_statement_locator).text
+
+        @property
+        def copyright(self):
+            """Return the OpenStax copyright statement."""
+            return self.find_element(*self._copyright_statement_locator).text
+
+        @property
+        def ap_statement(self):
+            """Return the AP statement."""
+            return self.find_element(*self._ap_statement_locator).text
+
+    class Social(Region):
+        """OpenStax social program Links."""
 
         _facebook_locator = (By.CLASS_NAME, 'facebook')
         _twitter_locator = (By.CLASS_NAME, 'twitter')
@@ -136,20 +142,20 @@ class Footer(Region):
         def go_to_facebook(self):
             """Go to OpenStax's Facebook page."""
             self.find_element(*self._facebook_locator).click()
-            sleep(1)
+            sleep(1.0)
             from pages.facebook.home import Facebook
             return Facebook(self.driver)
 
         def go_to_twitter(self):
             """Go to OpenStax's Twitter page."""
             self.find_element(*self._twitter_locator).click()
-            sleep(1)
+            sleep(1.0)
             from pages.twitter.home import Twitter
             return Twitter(self.driver)
 
         def go_to_linkedin(self):
             """Go to OpenStax's LinkedIn company page."""
             self.find_element(*self._linkedin_locator).click()
-            sleep(1)
+            sleep(1.0)
             from pages.linkedin.home import LinkedIn
             return LinkedIn(self.driver)
--- regions/web/web_nav.py
+++ regions/web/web_nav.py
@@ -3,57 +3,310 @@
 from time import sleep
 
 from pypom import Region
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 
 class WebNav(Region):
     """Website navbar region."""
 
-    _about_us_locator = (By.CSS_SELECTOR, "a[href*='about']")
-    _supporters_locator = (By.CSS_SELECTOR, "a[href*='foundation']")
-    _blog_locator = (By.CSS_SELECTOR, "a[href*='blog']")
-    _give_locator = (By.CSS_SELECTOR, "a[href*='give']")
-    _help_locator = (By.CSS_SELECTOR, "a[href*='help']")
-    _rice_locator = (By.CLASS_NAME, "rice-logo")
+    _root_locator = (By.CLASS_NAME, 'nav')
+    _openstax_logo_locator = (By.CSS_SELECTOR, '.os-logo > a')
+    _subjects_dropdown_locator = (By.CLASS_NAME, 'subjects-dropdown')
+    _technology_dropdown_locator = (By.CLASS_NAME, 'technology-dropdown')
+    _what_we_do_dropdown_locator = (By.CLASS_NAME, 'what-we-do-dropdown')
+    _user_menu_locator = (By.CLASS_NAME, 'login')
 
-    def go_to_about_us(self):
-        """Goes to the about us page."""
-        self.find_element(*self._about_us_locator).click()
-        sleep(1)
-        from pages.web.about import AboutUs
-        return AboutUs(self.driver)
+    @property
+    def is_displayed(self):
+        """Return True if the site navigation bar is visible."""
+        return self.find_element(*self._openstax_logo_locator).is_displayed
 
-    def go_to_supporters(self):
-        """Goes to the supporters page."""
-        self.find_element(*self._supporters_locator).click()
-        sleep(1)
-        from pages.web.supporters import Supporters
-        return Supporters(self.driver)
+    def go_home(self):
+        """Return to the home page by clicking on the OpenStax logo."""
+        self.find_element(*self._openstax_logo_locator).click()
+        sleep(1.0)
+        from pages.web.home import WebHome
+        return WebHome(self.driver)
 
-    def go_to_blog(self):
-        """Goes to the blog page."""
-        self.find_element(*self._blog_locator).click()
-        sleep(1)
-        from pages.web.blog import Blog
-        return Blog(self.driver)
+    @property
+    def subjects(self):
+        """Access the Subject dropdown menu."""
+        region_root = self.find_element(*self._subjects_dropdown_locator)
+        return self.Subjects(self, region_root)
 
-    def go_to_give(self):
-        """Goes to the give page."""
-        self.find_element(*self._give_locator).click()
-        sleep(1)
-        from pages.web.give import Give
-        return Give(self.driver)
+    @property
+    def technology(self):
+        """Access the Technology dropdown menu."""
+        region_root = self.find_element(*self._technology_dropdown_locator)
+        return self.Technology(self, region_root)
 
-    def go_to_help(self):
-        """Goes to the help page."""
-        self.find_element(*self._help_locator).click()
-        sleep(1)
-        from pages.salesforce.home import Salesforce
-        return Salesforce(self.driver)
+    @property
+    def openstax(self):
+        """Access the What we do dropdown menu."""
+        region_root = self.find_element(*self._what_we_do_dropdown_locator)
+        return self.WhatWeDo(self, region_root)
 
-    def go_to_rice(self):
-        """Goes to the rice home page."""
-        self.find_element(*self._rice_locator).click()
-        sleep(1)
-        from pages.rice.home import Rice
-        return Rice(self.driver)
+    @property
+    def login(self):
+        """Access the Login option or menu."""
+        return self.Login(self)
+
+    class Subjects(Region):
+        """The Subject navigation menu dropdown."""
+
+        _open_menu_locator = (By.CSS_SELECTOR, '[href="."]')
+        _all_option_locator = (By.CSS_SELECTOR, '[href$=subjects]')
+        _math_option_locator = (By.CSS_SELECTOR, '[href$=math]')
+        _science_option_locator = (By.CSS_SELECTOR, '[href$=science]')
+        _social_sciences_option_locator = (By.CSS_SELECTOR,
+                                           '[href$=social-sciences]')
+        _humanities_option_locator = (By.CSS_SELECTOR, '[href$=humanities]')
+        _ap_option_locator = (By.CSS_SELECTOR, '[href$=AP]')
+
+        @property
+        def open(self):
+            """Select the Subjects menu."""
+            self.find_element(*self._open_menu_locator).click()
+            sleep(0.5)
+            return self
+
+        @property
+        def all(self):
+            """Return the all subjects link."""
+            return self.find_element(*self._all_option_locator)
+
+        def view_all(self):
+            """View all book subjects."""
+            return self.open._selection_helper(
+                self._all_option_locator)
+
+        @property
+        def math(self):
+            """Return the math subjects link."""
+            return self.find_element(*self._math_option_locator)
+
+        def view_math(self):
+            """View all math books."""
+            return self.open._selection_helper(
+                self._math_option_locator)
+
+        @property
+        def science(self):
+            """Return the science subjects link."""
+            return self.find_element(*self._science_option_locator)
+
+        def view_science(self):
+            """View all science books."""
+            return self.open._selection_helper(
+                self._science_option_locator)
+
+        @property
+        def social_science(self):
+            """Return the social science subjects link."""
+            return self.find_element(*self._social_science_option_locator)
+
+        def view_social_science(self):
+            """View all social science books."""
+            return self.open._selection_helper(
+                self._social_sciences_option_locator)
+
+        @property
+        def ap(self):
+            """Return the AP subjects link."""
+            return self.find_element(*self._ap_option_locator)
+
+        def view_ap(self):
+            """View all AP books."""
+            return self.open._selection_helper(
+                self._ap_option_locator)
+
+        def _selection_helper(self, locator):
+            """Select the corresponding option."""
+            self.open.find_element(*locator).click()
+            sleep(1.0)
+            from pages.web.subjects import Subjects
+            return Subjects(self.driver)
+
+    class Technology(Region):
+        """The Technology navigation menu dropdown."""
+
+        _open_menu_locator = (By.CSS_SELECTOR, '[href="."]')
+        _technology_option_locator = (By.CSS_SELECTOR, '[href$=technology]')
+        _tutor_option_locator = (By.CSS_SELECTOR, '[href$=openstax-tutor]')
+        _partners_option_locator = (By.CSS_SELECTOR, '[href$=partners]')
+
+        @property
+        def open(self):
+            """Select the Technology menu."""
+            self.find_element(*self._open_menu_locator).click()
+            sleep(0.5)
+            return self
+
+        @property
+        def technology(self):
+            """Return the technology link."""
+            return self.find_element(*self._technology_option_locator)
+
+        def view_technology(self):
+            """View the technology page."""
+            self.open.technology.click()
+            sleep(1.0)
+            from pages.web.technology import Technology
+            return Technology(self.driver)
+
+        @property
+        def tutor(self):
+            """Return the OpenStax Tutor link."""
+            return self.find_element(*self._tutor_option_locator)
+
+        def view_tutor(self):
+            """View the OpenStax Tutor beta marketing page."""
+            self.open.tutor.click()
+            sleep(1.0)
+            from pages.web.tutor_marketing import TutorMarketing
+            return TutorMarketing(self.driver)
+
+        @property
+        def partners(self):
+            """Return the OpenStax partners link."""
+            return self.find_element(*self._partners_option_locator)
+
+        def view_partners(self):
+            """View the OpenStax partners page."""
+            self.open.partners.click()
+            sleep(1.0)
+            from pages.web.partners import Partners
+            return Partners(self.driver)
+
+    class WhatWeDo(Region):
+        """The What we do navigation menu dropdown."""
+
+        _open_menu_locator = (By.CSS_SELECTOR, '[href="."]')
+        _about_us_option_locator = (By.CSS_SELECTOR, '[href$=about]')
+        _team_option_locator = (By.CSS_SELECTOR, '[href$=team]')
+        _research_option_locator = (By.CSS_SELECTOR, '[href$=research]')
+
+        @property
+        def open(self):
+            """Select the What we do menu."""
+            self.find_element(*self._open_menu_locator).click()
+            sleep(0.5)
+            return self
+
+        @property
+        def about_us(self):
+            """Return the About Us link."""
+            return self.find_element(*self._about_us_option_locator)
+
+        def view_about_us(self):
+            """View the About Us page."""
+            self.open.about_us.click()
+            sleep(1.0)
+            from pages.web.about_us import AboutUs
+            return AboutUs(self.driver)
+
+        @property
+        def team(self):
+            """Return the OpenStax team link."""
+            return self.find_element(*self._team_option_locator)
+
+        def view_team(self):
+            """View the OpenStax team page."""
+            self.open.team.click()
+            sleep(1.0)
+            from pages.web.team import Team
+            return Team(self.driver)
+
+        @property
+        def research(self):
+            """Return the research team link."""
+            return self.find_element(*self._research_option_locator)
+
+        def view_research(self):
+            """View the OpenStax researcher page."""
+            self.open.research.click()
+            sleep(1.0)
+            from pages.web.research import Research
+            return Research(self.driver)
+
+    class Login(Region):
+        """The login option and menu."""
+
+        _log_in_link_locator = (By.LINK_TEXT, 'Login')
+        _logged_in_locator = (By.CLASS_NAME, 'login-dropdown')
+        _open_menu_locator = (By.CSS_SELECTOR, '[href="."]')
+        _user_name_locator = (By.PARTIAL_LINK_TEXT, 'Hi ')
+        _profile_link_locator = (By.CSS_SELECTOR, '[href$=profile]')
+        _openstax_tutor_link_locator = (By.LINK_TEXT, 'OpenStax Tutor')
+        _log_out_link_locator = (By.CSS_SELECTOR, '[href*=logout]')
+
+        @property
+        def login(self):
+            """Return the log in link."""
+            return self.find_element(*self._log_in_link_locator)
+
+        def log_in(self, user, password):
+            """Log a user into the website."""
+            accounts = self.login.click()
+            sleep(1.0)
+            accounts.log_in(user, password)
+            from pages.web.home import WebHome
+            return WebHome(accounts.driver)
+
+        @property
+        def logged_in(self):
+            """Return True if a user is logged into Web."""
+            try:
+                self.find_element(*self._logged_in_locator)
+            except NoSuchElementException:
+                return False
+            return True
+
+        @property
+        def open(self):
+            """Select the user menu."""
+            self.find_element(*self._open_menu_locator).click()
+            sleep(0.5)
+            return self
+
+        @property
+        def name(self):
+            """Return the user's first name as shown in the menu bar."""
+            return self.find_element(*self._user_name_locator).text[3:].strip()
+
+        @property
+        def profile(self):
+            """Return the user profile link."""
+            return self.find_element(*self._profile_link_locator)
+
+        def view_profile(self):
+            """View the user's account profile on Accounts."""
+            self.open.profile.click()
+            sleep(1.0)
+            from pages.accounts.profile import Profile
+            return Profile(self.driver)
+
+        @property
+        def tutor(self):
+            """Return the OpenStax Tutor Beta link."""
+            return self.find_element(*self._tutor_option_locator)
+
+        def view_tutor(self):
+            """View OpenStax Tutor Beta for the current user."""
+            self.open.tutor.click()
+            sleep(1.0)
+            from pages.tutor.dashboard import Dashboard
+            return Dashboard(self.driver)
+
+        @property
+        def logout(self):
+            """Return the log out link."""
+            return self.find_element(*self._log_out_link_locator)
+
+        def log_out(self):
+            """Log the current user out."""
+            self.open.logout.click()
+            sleep(1.0)
+            from pages.web.home import WebHome
+            return WebHome(self.driver)
