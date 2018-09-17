45a0abc3e5b951916e394931355c512a959dc841
Greg Fitch <greg.fitch@rice.edu>
2018-09-17 17:36:14 -0500

    adoption form loads

--- pages/web/adoption.py
+++ pages/web/adoption.py
@@ -1,5 +1,33 @@
 """The book adoption form."""
 
-# Adoption page and form
+from selenium.webdriver.common.by import By
 
-# Adoption Confirmation page
+from pages.web.base import WebBase
+
+
+class Adoption(WebBase):
+    """The adoption form."""
+
+    URL_TEMPLATE = '/adoption'
+
+    _loaded_locator = (By.CLASS_NAME, 'page-loaded')
+    _drop_down_menu_locator = (By.CLASS_NAME, 'proxy-select')
+
+    @property
+    def loaded(self):
+        """Wait until the form is displayed."""
+        return (self.find_element(*self._loaded_locator).is_displayed and
+                self.find_element(*self._drop_down_menu_locator).is_displayed)
+
+
+class AdoptionConfirmation(WebBase):
+    """The adoption confirmation page."""
+
+    URL_TEMPLATE = '/adoption-confirmation'
+
+    _confirmation_locator = (By.CLASS_NAME, 'adoption-confirmation')
+
+    @property
+    def loaded(self):
+        """Wait until the confirmation is displayed."""
+        return self.find_element(*self._confirmation_locator).is_displayed
--- tests/web/test_web_adoption.py
+++ tests/web/test_web_adoption.py
@@ -1,1 +1,18 @@
 """Test the adoption form."""
+
+from pages.web.adoption import Adoption
+from tests.markers import nondestructive, test_case, web
+
+
+@test_case('C210385')
+@nondestructive
+@web
+def test_adoption_form_loads(web_base_url, selenium):
+    """Test the for loading."""
+    # GIVEN: a user viewing the adoption page
+    adoption = Adoption(selenium, web_base_url).open()
+
+    # WHEN:
+
+    # THEN: the form is displayed
+    assert(adoption.loaded)
