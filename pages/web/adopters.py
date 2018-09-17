341621caa02fc2ec753b9fa0c0f972fe26c3e344
Greg Fitch <greg.fitch@rice.edu>
2018-09-17 17:03:12 -0500

    adopters page loading (#149)

--- pages/web/adopters.py
+++ pages/web/adopters.py
@@ -1,1 +1,25 @@
 """OpenStax material adopters."""
+
+from selenium.webdriver.common.by import By
+
+from pages.web.base import WebBase
+
+
+class Adopters(WebBase):
+    """Adopters page."""
+
+    URL_TEMPLATE = '/adopters'
+
+    _loaded_locator = (By.CLASS_NAME, 'page-loaded')
+    _institution_locator = (By.CSS_SELECTOR, 'main li')
+
+    @property
+    def loaded(self):
+        """Wait until the institution list is displayed."""
+        return (self.find_element(*self._loaded_locator).is_displayed
+                and self.find_element(*self._institution_locator).is_displayed)
+
+    @property
+    def adopters(self):
+        """Return the list of adopters."""
+        return self.find_elements(*self._institution_locator)
--- tests/web/test_web_adopters.py
+++ tests/web/test_web_adopters.py
@@ -1,1 +1,18 @@
 """Test the adopters list."""
+
+from pages.web.adopters import Adopters
+from tests.markers import nondestructive, test_case, web
+
+
+@test_case('C210384')
+@nondestructive
+@web
+def test_adopting_institutions_are_listed(web_base_url, selenium):
+    """Test for the presence of adopting institutions."""
+    # GIVEN: a user viewing the adopters page
+    adopters = Adopters(selenium, web_base_url).open()
+
+    # WHEN:
+
+    # THEN: a list of institutions is displayed
+    assert(len(adopters.adopters) > 0), 'No institutions found'
