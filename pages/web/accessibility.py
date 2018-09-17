6b986badf1734c0bd4518f6cd17ff2b3ea6f40e1
Greg Fitch <greg.fitch@rice.edu>
2018-09-17 16:37:27 -0500

    accessibility statement loads (#148)

--- pages/web/accessibility.py
+++ pages/web/accessibility.py
@@ -1,1 +1,23 @@
 """The OpenStax Web accessibility statement page."""
+
+from selenium.webdriver.common.by import By
+
+from pages.web.base import WebBase
+
+
+class Accessibility(WebBase):
+    """The accessibility statement."""
+
+    URL_TEMPLATE = '/accessibility-statement'
+
+    _section_locator = (By.TAG_NAME, 'h2')
+
+    @property
+    def loaded(self):
+        """Wait until the statement is displayed."""
+        return self.find_element(*self._section_locator).is_displayed
+
+    @property
+    def sections(self):
+        """Access the section headers."""
+        return self.find_elements(*self._section_locator)
