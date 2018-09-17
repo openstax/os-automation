a1fe1069f74d14266abf5b88f12dc436118d12df
Greg Fitch <greg.fitch@rice.edu>
2018-09-17 09:15:39 -0500

    who we are section tests pass

--- pages/web/faq.py
+++ pages/web/faq.py
@@ -1,1 +1,18 @@
 """The frequently asked questions page."""
+
+from selenium.webdriver.common.by import By
+
+from pages.web.base import WebBase
+
+
+class FAQ(WebBase):
+    """The OpenStax frequently asked questions."""
+
+    URL_TEMPLATE = '/faq'
+
+    _main_content_locator = (By.CLASS_NAME, 'page-loaded')
+
+    @property
+    def loaded(self):
+        """Return True if the hero banner is found."""
+        return self.find_element(*self._main_content_locator).is_displayed
--- pages/web/partners.py
+++ pages/web/partners.py
@@ -1,1 +1,18 @@
 """OpenStax Partners."""
+
+from selenium.webdriver.common.by import By
+
+from pages.web.base import WebBase
+
+
+class Partners(WebBase):
+    """The OpenStax Partners page."""
+
+    URL_TEMPLATE = '/partners'
+
+    _banner_locator = (By.CLASS_NAME, 'partners-page')
+
+    @property
+    def loaded(self):
+        """Return True if the hero banner is found."""
+        return self.find_element(*self._banner_locator).is_displayed
