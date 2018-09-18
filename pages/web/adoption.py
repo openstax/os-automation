ad703aeca39baf552607df3e7cc9cef4e099adfd
Greg Fitch <greg.fitch@rice.edu>
2018-09-18 09:21:28 -0500

    add link to interest test

--- pages/web/adoption.py
+++ pages/web/adoption.py
@@ -1,5 +1,7 @@
 """The book adoption form."""
 
+from time import sleep
+
 from selenium.webdriver.common.by import By
 
 from pages.web.base import WebBase
@@ -12,12 +14,22 @@ class Adoption(WebBase):
 
     _loaded_locator = (By.CLASS_NAME, 'page-loaded')
     _drop_down_menu_locator = (By.CLASS_NAME, 'proxy-select')
+    _interest_form_link_locator = (By.CSS_SELECTOR, '[href$=interest]')
 
     @property
     def loaded(self):
         """Wait until the form is displayed."""
         return (self.find_element(*self._loaded_locator).is_displayed and
                 self.find_element(*self._drop_down_menu_locator).is_displayed)
+
+    def go_to_interest(self):
+        """Switch to the interest form."""
+        self.wait.until(
+            lambda _: self.find_element(*self._interest_form_link_locator)
+        ).click()
+        sleep(1.0)
+        from pages.web.interest import Interest
+        return Interest(self.driver)
 
 
 class AdoptionConfirmation(WebBase):
--- tests/web/test_web_adoption.py
+++ tests/web/test_web_adoption.py
@@ -8,7 +8,7 @@ from tests.markers import nondestructive, test_case, web
 @nondestructive
 @web
 def test_adoption_form_loads(web_base_url, selenium):
-    """Test the for loading."""
+    """Test the form loading."""
     # GIVEN: a user viewing the adoption page
     adoption = Adoption(selenium, web_base_url).open()
 
@@ -16,3 +16,18 @@ def test_adoption_form_loads(web_base_url, selenium):
 
     # THEN: the form is displayed
     assert(adoption.loaded)
+
+
+@test_case('C210386')
+@nondestructive
+@web
+def test_adoption_form_links_to_the_interest_form(web_base_url, selenium):
+    """Test the cross-form link."""
+    # GIVEN: a user viewing the adoption page
+    adoption = Adoption(selenium, web_base_url).open()
+
+    # WHEN: they click on the "interest form" link
+    interest = adoption.go_to_interest()
+
+    # THEN: the interest page is displayed
+    assert(interest.loaded)
