8f8b11ecab35ce93c1a15e7986b691d0016231c6
Greg Fitch <greg.fitch@rice.edu>
2018-12-04 16:18:03 -0600

    add rest of legal object and ToU tests

--- pages/web/legal.py
+++ pages/web/legal.py
@@ -1,5 +1,7 @@
 """The legal / intellectual property frequently asked questions page."""
 
+from time import sleep
+
 from selenium.webdriver.common.by import By
 
 from pages.web.base import WebBase
@@ -9,22 +11,40 @@ from utils.utilities import Utility
 class LegalBase(WebBase):
     """The base page for the legal document pages."""
 
-    _heading_locator = (By.CSS_SELECTOR, 'h1')
-    _content_locator = (By.CSS_SELECTOR, '[data-html*=content]')
+    _heading_locator = (By.CSS_SELECTOR, '#main h1')
+    _content_locator = (By.CSS_SELECTOR, '#main [data-html*=content]')
+    _text_locator = (By.CSS_SELECTOR, 'p')
 
     @property
     def loaded(self):
         """Return True when the content is available."""
-        return Utility.has_children(self.content)
+        return sleep(1) or Utility.has_children(self.content)
+
+    @property
+    def heading(self):
+        """Return the heading."""
+        return self.find_element(*self._heading_locator)
 
     @property
     def content(self):
         """Return the content wrapper."""
         return self.find_element(*self._content_locator)
 
+    @property
+    def title(self):
+        """Return the heading text."""
+        return self.heading.text.strip()
+
+    @property
+    def text(self):
+        """Return the body text."""
+        return [paragraph.text.strip()
+                for paragraph
+                in self.content.find_elements(*self._text_locator)]
+
     def is_displayed(self):
         """Return True if the heading is displayed."""
-        return self.find_element(*self._heading_locator).is_displayed()
+        return self.heading.is_displayed()
 
 
 class License(LegalBase):
--- tests/web/test_web_terms.py
+++ tests/web/test_web_terms.py
@@ -1,1 +1,23 @@
 """Tests for the OpenStax Web terms of service and use."""
+
+from pages.web.home import WebHome
+from tests.markers import nondestructive, test_case, web
+
+
+@test_case('C210376', 'C210377')
+@nondestructive
+@web
+def test_the_terms_of_use_load(web_base_url, selenium):
+    """The Terms of Use page loads."""
+    # GIVEN: a web browser
+    home = WebHome(selenium, web_base_url).open()
+
+    # WHEN: a user opens the terms webpage
+    terms = home.footer.directory.view_the_terms_of_use()
+
+    # THEN: the page loads
+    # AND:  there are multiple sections with text
+    assert(terms.is_displayed())
+    assert('/tos' in terms.location)
+    assert(terms.title == 'Terms of Use')
+    assert(terms.text)
