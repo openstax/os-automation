5e0c2d3338a5ecfe2de193e64b1fbd955a7733dd
Greg Fitch <greg.fitch@rice.edu>
2018-12-10 10:56:57 -0600

    Web Press page

--- pages/web/press.py
+++ pages/web/press.py
@@ -19,14 +19,15 @@ class Press(WebBase):
     @property
     def loaded(self):
         """Return True when the four root elements are found."""
-        locator = (
+        selector = (
+            By.CSS_SELECTOR,
             '{hero} , {press} , {sidebar} , {news}'
             .format(hero=self._hero_quote_locator[1],
                     press=self._press_releases_locator[1],
                     sidebar=self._sidebar_locator[1],
                     news=self._news_mentions_locator[1]))
         try:
-            merged = self.find_elements(By.CSS_SELECTOR, locator)
+            merged = self.find_elements(*selector)
             return len(merged) == 4
         except WebDriverException:
             return False
