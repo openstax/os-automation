f2bf42f1ab63a229e1f0b18194de64278e828d1a
Greg Fitch <greg.fitch@rice.edu>
2018-12-17 13:59:10 -0600

    add careers page object and tests (#351)

--- pages/web/careers.py
+++ pages/web/careers.py
@@ -1,10 +1,12 @@
 """The OpenStax jobs board."""
 
+import re
 from time import sleep
 
 from selenium.webdriver.common.by import By
 
 from pages.web.base import WebBase
+from utils.utilities import Utility, go_to_
 
 
 class Careers(WebBase):
@@ -12,8 +14,11 @@ class Careers(WebBase):
 
     URL_TEMPLATE = '/careers'
 
-    _banner_locator = (By.CSS_SELECTOR, '#main h1')
+    _banner_locator = (By.CSS_SELECTOR, 'h1')
+    _subheading_locator = (
+        By.CSS_SELECTOR, '[data-html=content] p:first-child')
     _careers_content_locator = (By.CSS_SELECTOR, '[data-html=content]')
+    _job_list_locator = (By.CSS_SELECTOR, '[data-html=content] p:nth-child(5)')
 
     @property
     def loaded(self):
@@ -25,3 +30,60 @@ class Careers(WebBase):
     def is_displayed(self):
         """Return True if the heading is displayed."""
         return self.find_element(*self._banner_locator).is_displayed()
+
+    @property
+    def job_list(self):
+        """Return the content within the open positions element."""
+        return (self.find_element(*self._job_list_locator)
+                .get_attribute('innerHTML'))
+
+    @property
+    def jobs(self):
+        """Access the available jobs."""
+        segments = (re.sub(r'(<br[\/ ]{0,2}>){2}', '|||', self.job_list)
+                    .split('|||'))
+        return [self.Job(self, text) for text in segments]
+
+    class Job():
+        """A job entry."""
+
+        def __init__(self, page, job_string):
+            """Initialize an open job position object."""
+            self.page = page
+            try:
+                href = re.search(r'href=\"[\w\d\/\:\*\.]*\"', job_string)
+                self._url = href.group().split('"')[1] if href else ''
+            except AttributeError:
+                self._url = ''
+            try:
+                title = (re.search(r'<b>[^(<\/)]*<\/b>', job_string)
+                         .group().split('>', 1)[-1])
+                self._title = re.sub(r'<b>|<\/b>', '', title)
+            except AttributeError:
+                self._title = ''
+            try:
+                self._description = re.split(r'<\/b>|<br>', job_string)[-1]
+            except AttributeError:
+                self._description = ''
+
+        @property
+        def title(self):
+            """Return the job title."""
+            return self._title
+
+        @property
+        def description(self):
+            """Return the job summary."""
+            return self._description
+
+        @property
+        def url(self):
+            """Return the Rice Jobs posting URL."""
+            return self._url
+
+        def view_position(self):
+            """Click on the position name to view the Rice Jobs board."""
+            button = self.page.find_element(By.LINK_TEXT, self.title)
+            Utility.switch_to(self.page.driver, element=button)
+            from pages.rice.jobs import RiceJobs
+            return go_to_(RiceJobs(self.page.driver))
--- tests/web/test_web_careers.py
+++ tests/web/test_web_careers.py
@@ -1,1 +1,30 @@
 """Test the OpenStax jobs page."""
+
+from pages.web.home import WebHome
+from tests.markers import nondestructive, test_case, web
+from utils.utilities import Utility
+
+
+@test_case('C210408', 'C210409', 'C210410')
+@nondestructive
+@web
+def test_open_positions_are_available(web_base_url, selenium):
+    """Open positions are listed."""
+    # GIVEN: a user viewing the careers page
+    home = WebHome(selenium, web_base_url).open()
+    careers = home.footer.directory.view_openstax_career_opportunities()
+
+    # WHEN:
+
+    # THEN: the position title is displayed
+    # AND:  a brief description of the position is displayed
+    # AND:  a link to the job post is available
+    # AND:  the job posting on Rice Jobs is verified
+    assert(careers.is_displayed())
+    assert('careers' in careers.location)
+    for position in careers.jobs:
+        assert(position.title)
+        assert(position.description)
+        assert(position.url)
+        assert(Utility.test_url_and_warn(url=position.url,
+                                         message=position.title))
