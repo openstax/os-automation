7e829419b10a369b1269eedbc88d21fb0fb849ee
Greg Fitch <greg.fitch@rice.edu>
2019-04-17 14:51:37 -0500

    update doctext for the home page

--- pages/tutor/home.py
+++ pages/tutor/home.py
@@ -17,16 +17,31 @@ class TutorHome(TutorLoginBase):
 
     @property
     def title(self):
-        """Return the slogan text."""
+        """Return the slogan text.
+
+        :return: the OpenStax Tutor Beta marketing slogan
+        :rtype: str
+
+        """
         return self.find_element(*self._slogan_locator).text
 
     @property
     def description(self):
-        """Return the descriptive Tutor overview text."""
+        """Return the descriptive Tutor overview text.
+
+        :return: the overview description
+        :rtype: str
+
+        """
         return self.find_element(*self._description_locator).text
 
     def learn_more(self):
-        """Click on the 'LEARN MORE' link to view the marketing page."""
+        """Click on the 'LEARN MORE' link to view the marketing page.
+
+        :return: the OpenStax Tutor marketing page on openstax.org
+        :rtype: :py:class:`~pages.web.tutor.TutorMarketing`
+
+        """
         link = self.find_element(*self._learn_more_locator)
         url = link.get_attribute('href')
         Utility.click_option(self.driver, element=link)
@@ -34,13 +49,25 @@ class TutorHome(TutorLoginBase):
         return go_to_(TutorMarketing(self.driver, url))
 
     def go_to_log_in(self):
-        """Click the 'LOG IN' button."""
+        """Click the 'LOG IN' button.
+
+        :return: the Accounts log in page
+        :rtype: :py:class:`~pages.accounts.home.AccountsHome`
+
+        """
         self.find_element(*self._log_in_locator).click()
         from pages.accounts.home import AccountsHome
         return go_to_(AccountsHome(self.driver))
 
     def service_pass_through(self):
-        """Click the log in button for an active Accounts user."""
+        """Click the log in button for an active Accounts user.
+
+        :return: the user's dashboard
+        :rtype: :py:class:`~pages.tutor.dashboard.Dashboard`
+        :raises :py:class:`~selenium.common.exceptions.NoSuchElementException`:
+            if the login bar is not found
+
+        """
         try:
             self.find_element(*self._log_in_locator).click()
         except NoSuchElementException:
@@ -49,7 +76,12 @@ class TutorHome(TutorLoginBase):
         return go_to_(Dashboard(self.driver, self.base_url))
 
     def log_in(self, username, password):
-        """Log into Tutor with a specific user."""
+        """Log into Tutor with a specific user.
+
+        :return: the user's dashboard
+        :rtype: :py:class:`~pages.tutor.dashboard.Dashboard`
+
+        """
         accounts = self.go_to_log_in()
         accounts.service_log_in(username, password)
         self.wait.until(lambda _: self.logged_in)
@@ -58,7 +90,12 @@ class TutorHome(TutorLoginBase):
 
     @property
     def logged_in(self):
-        """Return True if the user is logged into Tutor."""
+        """Return True if the user is logged into Tutor.
+
+        :return: ``True`` if the user is logged into Tutor, else ``False``
+        :rtype: bool
+
+        """
         return 'dashboard' in self.location or 'course' in self.location
 
     class Footer(TutorLoginBase.Footer):
@@ -67,7 +104,13 @@ class TutorHome(TutorLoginBase):
         _gdpr_locator = (By.CSS_SELECTOR, '[href*=gdpr]')
 
         def view_gdpr(self):
-            """Click on the GDPR link to view the GDPR on Rice's webpage."""
+            """Click on the GDPR link to view the GDPR on Rice's webpage.
+
+        :return: the General Data Privacy Regulation page on Rice University's
+            webpage in a new tab
+        :rtype: :py:class:`~pages.rice.gdpr.GeneralDataPrivacyRegulation`
+
+        """
             link = self.find_element(*self._gdpr_locator)
             url = link.get_attribute('href')
             Utility.switch_to(self.driver, element=link)
