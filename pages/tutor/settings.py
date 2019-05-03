d75d3927e5beeffe83c11f9b03e560c0c8467a03
Greg Fitch <greg.fitch@rice.edu>
2019-05-03 13:57:22 -0500

    open pr for settings

---
+++ pages/tutor/settings.py
@@ -0,0 +1,14 @@
+"""The instructor's course settings page."""
+
+# from pypom import Region
+# from selenium.common.exceptions import NoSuchElementException
+# from selenium.webdriver.common.by import By
+
+from pages.tutor.base import TutorBase
+
+# from utils.tutor import TutorException
+# from utils.utilities import Utility
+
+
+class CourseSettings(TutorBase):
+    """The Tutor course settings page."""
