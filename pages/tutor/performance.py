272bd38b6e87780b898e4c03f7ac22b815affa6a
Greg Fitch <greg.fitch@rice.edu>
2019-05-07 10:17:32 -0500

    Performance Forecast object

---
+++ pages/tutor/performance.py
@@ -0,0 +1,16 @@
+"""The instructor's course settings page."""
+
+# from time import sleep
+
+# from pypom import Region
+# from selenium.common.exceptions import NoSuchElementException
+# from selenium.webdriver.common.by import By
+
+from pages.tutor.base import TutorBase
+
+# from utils.tutor import Tutor, TutorException
+# from utils.utilities import Utility, go_to_
+
+
+class PerformanceForecast(TutorBase):
+    """The performance forecast page."""
