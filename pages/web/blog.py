215ce8ba6ff3ccc7da3662ce71de6ae8bd48e015
Greg Fitch <greg.fitch@rice.edu>
2019-09-16 12:01:55 -0500

    add more detailed docstrings to the blog page object

--- pages/web/blog.py
+++ pages/web/blog.py
@@ -26,7 +26,12 @@ class Blog(WebBase):
 
     @property
     def loaded(self):
-        """Return True when all of the blog article images are loaded."""
+        """Return True when all of the blog article images are loaded.
+
+        :return: ``True`` when the blog article background images are loaded
+        :rtype: bool
+
+        """
         timer = 0
         repeat = 15.0
         pause = 0.25
@@ -48,47 +53,92 @@ class Blog(WebBase):
         return test
 
     def is_displayed(self):
-        """Return True if the blog pinned article is displayed."""
+        """Return True if the blog pinned article is displayed.
+
+        :return: ``True`` when the pinned blog article entry is displayed
+        :rtype: bool
+
+        """
         return self.find_element(*self._pinned_article_locator).is_displayed()
 
     @property
     def pinned(self):
-        """Access the pinned entry."""
+        """Access the pinned entry.
+
+        :return: the pinned blog entry tile
+        :rtype: :py:class:`~pages.web.blog.Blog.Tile`
+
+        """
         pinned_root = self.find_element(*self._pinned_article_locator)
         return self.Tile(self, pinned_root)
 
     def get_newsletter(self):
-        """Select the 'Sign Up' button."""
+        """Select the 'Sign Up' button.
+
+        :return: the OpenStax newsletter signup form in a new window
+        :rtype: :py:class:`~pages.web.newsletter.NewsletterSignup`
+
+        """
         Utility.switch_to(self.driver, self._newsletter_locator)
         from pages.web.newsletter import NewsletterSignup
         return go_to_(NewsletterSignup(self.driver, self.base_url))
 
     def get_rss(self):
-        """Click the RSS feed icon."""
+        """Click the RSS feed icon.
+
+        :return: None
+
+        """
         Utility.switch_to(self.driver, self._rss_locator)
         if self.driver.capabilities.get('browserName').lower() != 'safari':
             self.wait.until(lambda _: 'about:blank' not in self.location)
-        return self.driver
 
     def test_rss_feed(self):
-        """Query the RSS feed URL."""
+        """Query the RSS feed URL.
+
+        :return: the result from a HEAD request to the RSS feed
+        :rtype: :py:class:`~requests.Response`
+
+        """
         from requests import head
         return head(self.find_element(*self._rss_locator)
                     .get_attribute('href'))
 
     def view_post(self, title=None, index=None):
-        """Open a blog entry to view the full article."""
+        """Open a blog entry to view the full article.
+
+        If no title or index is provided, return a random blog article
+
+        :param str title: (optional) select an article to view by its title
+        :param int index: (optional) select an article by it list index
+        :return: a blog article
+        :rtype: :py:class:`~pages.web.blog.Article`
+
+        """
+        # select an article by its title
         if title:
             for article in self.articles:
                 if article.title == title:
                     return article.view()
+
+        # or select an article by its position with 0 being the newest and n-1
+        # being the oldest
         if index:
             return self.other_posts[index].view()
+
+        # or select a random article
         return self.articles[Utility.random(0, len(self.articles) - 1)].view()
 
     @property
     def articles(self):
-        """Return a list of articles."""
+        """Return a list of articles.
+
+        Include the pinned article, if it exists.
+
+        :return: a list of available blog entries
+        :rtype: list(:py:class:`~pages.web.blog.Article`)
+
+        """
         try:
             self.find_element(*self._pinned_article_locator)
             return [self.pinned] + self.other_posts
@@ -97,9 +147,15 @@ class Blog(WebBase):
 
     @property
     def other_posts(self):
-        """Return the other blog entry tiles."""
+        """Return the other blog entry tiles.
+
+        :return: a list of blog entries, excluding the pinned article
+        :rtype: list(:py:class:`~pages.web.blog.Article`)
+
+        """
         return [self.Tile(self, tile)
-                for tile in self.find_elements(*self._other_posts_locator)]
+                for tile
+                in self.find_elements(*self._other_posts_locator)]
 
     class Tile(Region):
         """A blog entry overview tile."""
@@ -113,24 +169,48 @@ class Blog(WebBase):
         _date_locator = (By.CSS_SELECTOR, '.date')
 
         def is_displayed(self):
-            """Return True if the tile has children."""
+            """Return True if the tile has children.
+
+            :return: ``True`` if the tile is displayed and the tile has loaded
+                child elements (it entered the viewable window for at least 0.1
+                seconds)
+            :rtype: bool
+
+            """
             return (self.root.is_displayed() and
                     Utility.has_children(self.root))
 
         @property
         def image(self):
-            """Return the tile picture element."""
+            r"""Return the tile picture element.
+
+            :return: the tile's banner image element
+            :rtype: :py:class:`~selenium.webdriver.remote \
+                              .webelement.WebElement`
+
+            """
             return self.find_element(*self._image_locator)
 
         @property
         def title(self):
-            """Return the article title."""
+            """Return the article title.
+
+            :return: the blog article title
+            :rtype: str
+
+            """
             return (self.find_element(*self._title_locator)
                     .get_attribute('textContent'))
 
         @property
         def excerpt(self):
-            """Return the article excerpt or subheading."""
+            """Return the article excerpt or subheading.
+
+            :return: the trimmed article excerpt or the article subheading plus
+                the excerpt
+            :rtype: str
+
+            """
             subheading = self.find_elements(*self._subheading_locator)
             content = subheading[0].get_attribute('textContent').strip() \
                 if subheading else ''
@@ -139,7 +219,12 @@ class Blog(WebBase):
             return f"{content} {excerpt}".strip()
 
         def view(self):
-            """Open the article."""
+            """Open the article.
+
+            :return: the full blog entry article
+            :rtype: :py:class:`~pages.web.blog.Article`
+
+            """
             link = self.find_element(*self._read_more_locator)
             article = link.get_attribute('href').split('/')[-1]
             Utility.click_option(self.driver, element=link)
@@ -148,16 +233,29 @@ class Blog(WebBase):
 
         @property
         def authors(self):
-            """Return the article authors and editors."""
+            """Return the article authors and editors.
+
+            :return: the article author(s)/editor(s) as a single string
+            :rtype: str
+
+            """
             return self.find_element(*self._author_locator).text
 
         @property
         def date(self):
-            """Return the article publish date string."""
+            """Return the article publish date string.
+
+            :return: the article's publish date string
+            :rtype: str
+
+            """
             return self.find_element(*self._date_locator).text
 
         def get_date(self):
-            """Return a timezone-aware datetime of the publish date."""
+            """Return a timezone-aware datetime of the publish date.
+
+            :return: the article's publish date as a timezone-aware datetime
+            :rtype: :py:class:`~datetime.datetime"""
             return datetime.strptime(self.date + ' +0000', '%b %d, %Y %z')
 
 
@@ -171,14 +269,26 @@ class Article(Blog):
     _disqus_locator = (By.CSS_SELECTOR, '#disqus_thread')
 
     def wait_for_page_to_load(self):
-        """Override the page load function."""
+        """Override the page load function.
+
+        Wait for the other blog article tiles and the article banner to load.
+
+        :return: None
+
+        """
         super(Blog, self).wait_for_page_to_load()
         self.wait.until(
             lambda _: (self.find_element(*self._hero_banner_locator)
                        .is_displayed()))
 
     def is_displayed(self):
-        """Return True if the blog article banner image is displayed."""
+        """Return True if the blog article banner image is displayed.
+
+        :return: ``True`` if the blog article banner image and the Disqus
+            comments widget are displayed
+        :rtype: bool
+
+        """
         return (
             self.wait.until(expect.visibility_of_element_located(
                 self._hero_banner_locator)).is_displayed() and
@@ -188,22 +298,42 @@ class Article(Blog):
 
     @property
     def title(self):
-        """Return the article title."""
+        """Return the article title.
+
+        :return: the blog article title
+        :rtype: str
+
+        """
         return self.find_element(*self._article_title_locator).text.strip()
 
     @property
     def disqus(self):
-        """Return the Disqus iframe."""
+        """Return the Disqus iframe.
+
+        :return: the Disqus iframe element
+        :rtype: :py:class:`~selenium.webdriver.remote.webelement.WebElement`
+
+        """
         return self.find_element(*self._disqus_locator)
 
     def view_comments(self):
-        """Scroll to the Disqus comment section."""
+        """Scroll to the Disqus comment section.
+
+        :return: the blog article
+        :rtype: :py:class:`~pages.web.blog.Article`
+
+        """
         self.wait.until(lambda _: self.disqus.is_displayed())
         Utility.scroll_to(self.driver, element_locator=self._disqus_locator)
         return self
 
     def view_other_posts(self):
-        """Scroll to the other blog article tiles."""
+        """Scroll to the other blog article tiles.
+
+        :return: the blog article
+        :rtype: :py:class:`~pages.web.blog.Article`
+
+        """
         Utility.scroll_to(self.driver,
                           element_locator=self._other_posts_locator)
         return self
